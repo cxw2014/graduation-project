9275546223dbaaffd8bd0209084f710240f66f1b
caowen <caowen@oneapm.com>
2016-03-13 21:39:00 +0800

    new

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
+app.use(function(err,req,res,next){
+  res.status(404).send('Sorry cannot find that!');
+})
+
+app.use(function(err,req,res,next){
+  res.status(500).send('Something broke!');
+})
 
 // development error handler
 // will print stacktrace
